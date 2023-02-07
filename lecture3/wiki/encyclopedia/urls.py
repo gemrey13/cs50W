9b2fbab97376f5eda92c5c46f34cfff36514b53a
gemrey <gemreyranola@gmail.com>
2023-02-07 18:21:40 +0800

    Ongoing in ouput if the search is not register it will provide the list of entries with the substring of input

--- lecture3/wiki/encyclopedia/urls.py
+++ lecture3/wiki/encyclopedia/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 from . import views
 
-
+"""
+ When the form is submitted. It will submit here in the url with the name search
+ The search path will get the q in form using the function in views.search
+ The path for seach cannot be empty because it need a url to display the output
+"""
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/", views.search, name="search"),
     path("wiki/<str:title>", views.entry, name="entry"),
 ]
+
+
--- lecture3/wiki/encyclopedia/views.py
+++ lecture3/wiki/encyclopedia/views.py
@@ -10,19 +10,38 @@ def index(request):
         "entries": util.list_entries()
     })
 
+
+"""
+ This function will get the form with name of q and get the entry(util.get_entry()) based on the q
+ This function will render in encyclopedia/entry.html template
+"""
 def search(request):
-    query = request.GET.get('q')
-    if query:
-        result = util.get_entry(query)
-        html = markdown2.markdown(result)
-        return render(request, "encyclopedia/entry.html", {
-            "content": html
+    try:
+        query = request.GET.get('q')
+        if query:
+            result = util.get_entry(query)
+            html = markdown2.markdown(result)
+
+            return render(request, "encyclopedia/entry.html", {
+                "content": html
+            })
+
+    except TypeError:
+        query = request.GET.get('q')
+        result = []
+        lists = util.list_entries()
+        for list in lists:
+            if query in list:
+                result.append(list)
+        
+        return render(request, "encyclopedia/index.html", {
+           "entries": result
         })
-    
 
 def entry(request, title):
     text = util.get_entry(title)
     html = markdown2.markdown(text)
+
     return render(request, "encyclopedia/entry.html", {
         "content": html
     })
