6511780a2abcf088cfa253c2e5ffbd62c33580ca
gemrey <gemreyranola@gmail.com>
2023-02-09 10:12:46 +0800

    Test Success

--- lecture3/wiki/encyclopedia/tests.py
+++ lecture3/wiki/encyclopedia/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
 # Create your tests here.
+class TestView(TestCase):
+    def test_client(self):
+        self.client = Client()
+
+
+    def test_entry(self):
+        response = self.client.get('/wiki/css')
+        self.assertEqual(response.status_code, 200)
+
+    def test_newpage(self):
+        response = self.client.get('/newpage/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'encyclopedia/newpage.html')
+
+    def test_editEntry(self):
+        response = self.client.get('/edit/Javascript')
+        self.assertEqual(response.status_code, 200)
