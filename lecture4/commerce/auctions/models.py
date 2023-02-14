0dbb7e88beeeb53d7ee2b64871c47dfd254c3511
gemrey <gemreyranola@gmail.com>
2023-02-15 07:54:39 +0800

    Fixing Watchlist Feature

--- lecture4/commerce/auctions/models.py
+++ lecture4/commerce/auctions/models.py
@@ -50,9 +50,9 @@ class Comment(models.Model):
 
 class Watchlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    auctionitem = models.ForeignKey(AuctionListing, on_delete=models.CASCADE)
+    auctionitem = models.ManyToManyField(AuctionListing, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f'{self.user} - {self.auctionitem} - {self.created_at}'
+        return f'{self.user} - {self.auctionitem} :Watchlist'
 
